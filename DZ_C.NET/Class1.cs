df80c7b31a4107c109cee570a7816f1824b63bfa
Микола Матіос <mikolka09@gmail.com>
2020-09-18 02:44:22 +0300

    18.09.2020

---
+++ DZ_C.NET/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_C.NET
+{
+    partial class Car
+    {
+        public void Print()
+        {
+            Console.WriteLine("Созданный автомобиль: ");
+            Console.WriteLine();
+            Console.WriteLine($"Автомобиль МАРКА - {Brand}, Тип - {Type}, Вид топлива - {TypeGas}\n" +
+                              $"Объем двигателя - {Volume}, Кол-во лошадинных сил - {Horsepower} и " +
+                              $"Дата изготовления - {Date}");
+        }
+
+        public void Beep()
+        {
+            Console.WriteLine("Проверка сигнала!!!");
+            Console.Beep();
+        }
+
+        public void DateTime(ref int d)
+        {
+            Date = d;
+        }
+    }
+}
--- DZ_C.NET/Program.cs
+++ DZ_C.NET/Program.cs
@@ -1,45 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DZ_C.NET
 {
+    partial class Car
+    {
+        static string Type;
+        static string TypeGas;
+        private string Brand;
+        private float Volume;
+        private int Horsepower;
+        private int Date = 0;
+
+        public int MyDate
+        {
+            get { return Date; }
+            set { Date = value; }
+        }
+
+
+        public int MyHorsepower
+        {
+            get { return Horsepower; }
+            set { Horsepower = value; }
+        }
+
+        public float MyVolume
+        {
+            get { return Volume; }
+            set { Volume = value; }
+        }
+
+        public string MyBrand
+        {
+            get { return Brand; }
+            set { Brand = value; }
+        }
+
+        static Car()
+        {
+            Type = "Passenger";
+            TypeGas = "Dizel";
+        }
+
+        public Car():this("No Brand", 0, 0) {}
+
+        public Car(string b)
+        {
+            Brand = b;
+        }
+        
+        public Car(string b, float v, int h)
+        {
+            Brand = b;
+            Volume = v;
+            Horsepower = h;
+        }
+
+        public void Assembly()
+        {
+            Console.WriteLine("Производим сборку выбранной модели Автомобиля!!!");
+        }
+
+        public void Tests()
+        {
+            Console.WriteLine("Производим испытания Автомобиля: \n\n" +
+                              "1. Провнряем зажигание (заводим авто).\n" +
+                              "2. Проверяем в движении на минимальных скоростях.\n" +
+                              "3. Проверяем в движении на максимальных скоростях.\n" +
+                              "4. Проверяем торможение обычное и экстренное.\n" +
+                              "5. Остановка авто и составление заключения испытаний.\n");
+        }
+
+        public void Defective()
+        {
+            Console.WriteLine("Автомобиль не прошедший испытаний отправляется на выявление дефектов и доработку!!!");
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            //ВСТРЕЧА 1
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
 
-            //Задание 1
-            //int n = 0;  //количество введенных пробелов
-            //Console.WriteLine("Введите символы: ");
-            //while(!Console.ReadLine().StartsWith("."))  //Считывает символы пока не введем точку
-            //{
-            //    if (Console.ReadLine().StartsWith(" "))
-            //        n++;
-            //}
-            //Console.WriteLine($"Количество пробелов = {n}");
+            Car car1 = new Car("Audi", 2.8F, 300);
+            Car car2 = new Car("Reno", 2.1F, 200);
+            Car car3 = new Car("Fiat", 1.5F, 150);
+            Car car4 = new Car("Lada", 2.2F, 200);
+            Car car5 = new Car("Ford", 2.6F, 250);
+            Car car6 = new Car("Geely", 2.1F, 150);
 
-            //Задание 2
-            Console.WriteLine("Введите номер трамвайного билета (6-ть цифр): ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int n1, n2, n3, n4, n5, n6;
-            n6 = n % 10;
-            n5 = (n / 10) % 10;
-            n4 = (n / 100) % 10;
-            n3 = (n / 1000) % 10;
-            n2 = (n / 10000) % 10;
-            n1 = n / 100000;
-            if ((n1 + n2 + n3) == (n4 + n5 + n6))
-                Console.WriteLine("Данный билет является счастливым!!!");
-            else
-                Console.WriteLine("Данный билет не является счастливым!");
+            Car[] arr = new Car[6];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[0] = car1;
+                arr[1] = car2;
+                arr[2] = car3;
+                arr[3] = car4;
+                arr[4] = car5;
+                arr[5] = car6;
+            }
 
-            //Задание 3
-            char st = Convert.ToChar(Console.ReadLine());
-            
+            foreach (var item in arr)
+            {
+                item.Print();
+                Console.WriteLine();
+            }
+
+            int d = 2020;
+            car1.DateTime(ref d);
+            car1.Print();
+
+
+
+
+
 
             Console.Read();
         }
