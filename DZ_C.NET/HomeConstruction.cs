8925c6d6d0f1cf35548c385a472c68f72ddc0840
Микола Матіос <mikolka09@gmail.com>
2020-10-03 22:18:11 +0300

    03.10.2020

---
+++ DZ_C.NET/HomeConstruction.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_C.NET
+{
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IPart
+    {
+        void Building();
+    }
+
+    class House : IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("\t\t\tHOUSE BUILD!!!\n" +
+                              "\t\t\t--------------");
+            Console.WriteLine();
+            string domik = @"
+                           (   )
+                          (    )
+                           (    )
+                          (    )
+                            )  )
+                           (  (                  /\
+                            (_)                 /  \  /\
+                    ________[_]________      /\/    \/  \
+           /\      /\        ______    \    /   /\/\  /\/\
+          /  \    //_\       \    /\    \  /\/\/    \/    \
+   /\    / /\/\  //___\       \__/  \    \/
+  /  \  /\/    \//_____\       \ |[]|     \
+ /\/\/\/       //_______\       \|__|      \
+/      \      /XXXXXXXXXX\                  \
+        \    /_I_II  I__I_\__________________\
+               I_I|  I__I_____[]_|_[]_____I
+               I_II  I__I_____[]_|_[]_____I
+               I II__I  I     XXXXXXX     I
+            ~~~~~'   '~~~~~~~~~~~~~~~~~~~~~~~~";
+            Console.WriteLine(domik);
+
+        }
+    }
+
+    class Basement : House, IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("Building Basement!");
+        }
+    }
+
+    class Walls : House, IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("Building Walls!");
+        }
+    }
+
+    class Door : House, IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("Building Door!");
+        }
+    }
+
+    class Window : House, IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("Building Window!");
+        }
+    }
+
+    class Roof : House, IPart
+    {
+        public void Building()
+        {
+            Console.WriteLine("Building Roof!");
+        }
+    }
+
+    class Team : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Teams completed!");
+        }
+    }
+
+    class Worker : Team, IWorker
+    {
+
+        public void Work()
+        {
+            Console.WriteLine("Worker Building!");
+        }
+    }
+
+    class TeamLeader : Team, IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Form a Report:\n" +
+                              "--------------");
+        }
+    }
+}
--- DZ_C.NET/Program.cs
+++ DZ_C.NET/Program.cs
@@ -4,85 +4,11 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace DZ_C.NET
 {
-    partial class Car
-    {
-        static string Type;
-        static string TypeGas;
-        private string Brand;
-        private float Volume;
-        private int Horsepower;
-        private int Date = 0;
-
-        public int MyDate
-        {
-            get { return Date; }
-            set { Date = value; }
-        }
-
-
-        public int MyHorsepower
-        {
-            get { return Horsepower; }
-            set { Horsepower = value; }
-        }
-
-        public float MyVolume
-        {
-            get { return Volume; }
-            set { Volume = value; }
-        }
-
-        public string MyBrand
-        {
-            get { return Brand; }
-            set { Brand = value; }
-        }
-
-        static Car()
-        {
-            Type = "Passenger";
-            TypeGas = "Dizel";
-        }
-
-        public Car():this("No Brand", 0, 0) {}
-
-        public Car(string b)
-        {
-            Brand = b;
-        }
-        
-        public Car(string b, float v, int h)
-        {
-            Brand = b;
-            Volume = v;
-            Horsepower = h;
-        }
-
-        public void Assembly()
-        {
-            Console.WriteLine("Производим сборку выбранной модели Автомобиля!!!");
-        }
-
-        public void Tests()
-        {
-            Console.WriteLine("Производим испытания Автомобиля: \n\n" +
-                              "1. Провнряем зажигание (заводим авто).\n" +
-                              "2. Проверяем в движении на минимальных скоростях.\n" +
-                              "3. Проверяем в движении на максимальных скоростях.\n" +
-                              "4. Проверяем торможение обычное и экстренное.\n" +
-                              "5. Остановка авто и составление заключения испытаний.\n");
-        }
-
-        public void Defective()
-        {
-            Console.WriteLine("Автомобиль не прошедший испытаний отправляется на выявление дефектов и доработку!!!");
-        }
-
-    }
-
 
     class Program
     {
@@ -92,37 +18,148 @@ namespace DZ_C.NET
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
 
-            Car car1 = new Car("Audi", 2.8F, 300);
-            Car car2 = new Car("Reno", 2.1F, 200);
-            Car car3 = new Car("Fiat", 1.5F, 150);
-            Car car4 = new Car("Lada", 2.2F, 200);
-            Car car5 = new Car("Ford", 2.6F, 250);
-            Car car6 = new Car("Geely", 2.1F, 150);
-
-            Car[] arr = new Car[6];
-            for (int i = 0; i < arr.Length; i++)
+            Console.WriteLine("BUILDING HOUSE\n" +
+                              "--------------\n");
+            House[] house = new House[11];
+            Team[] teams = new Team[40];
+            for (int i = 0; i < teams.Length; i++)
             {
-                arr[0] = car1;
-                arr[1] = car2;
-                arr[2] = car3;
-                arr[3] = car4;
-                arr[4] = car5;
-                arr[5] = car6;
+                System.Random rand = new Random();
+                int k = rand.Next(0, 2);
+
+                if (k == 0)
+                    teams[i] = new Worker();
+                else
+                    teams[i] = new TeamLeader();
+                Thread.Sleep(50);
+            }
+            new Team().Work();
+            Console.WriteLine("=======================\n");
+            Console.WriteLine();
+
+            void Report(House[] dom, int hd)
+            {
+                if(hd==0)
+                    Console.WriteLine("Nothing built!");
+                else
+                {
+                    for (int i = 0; i < hd; i++)
+                    {
+                        if (dom[i].GetType() == new Basement().GetType())
+                            Console.WriteLine("Basement build!");
+                        else if (dom[i].GetType() == new Walls().GetType())
+                            Console.WriteLine("Walls build!");
+                        else if (dom[i].GetType() == new Door().GetType())
+                            Console.WriteLine("Door build!");
+                        else if (dom[i].GetType() == new Window().GetType())
+                            Console.WriteLine("Window build!");
+                        else if (dom[i].GetType() == new Roof().GetType())
+                            Console.WriteLine("Roof build!");
+                    }
+                }
+                
             }
 
-            foreach (var item in arr)
+            int w = 0;
+            int h = 0;
+            while (house[10] == null)
             {
-                item.Print();
-                Console.WriteLine();
+                if (house[0] == null)
+                {
+                    if (teams[w].GetType() == new Worker().GetType())
+                    {
+                        new Worker().Work();
+                        house[h] = new Basement();
+                        new Basement().Building();
+                        h++;
+                        w++;
+                    }
+                    else if (teams[w].GetType() == new TeamLeader().GetType())
+                    {
+                        new TeamLeader().Work();
+                        Report(house, h);
+                        w++;
+                    }
+
+                }
+                else if (teams[w].GetType() == new Worker().GetType())
+                {
+                    if (house[h - 1].GetType() == new Basement().GetType())
+                    {
+                        new Worker().Work();
+                        house[h] = new Walls();
+                        new Walls().Building();
+                        h++;
+                        w++;
+                    }
+                    else if (house[h - 1].GetType() == new Walls().GetType())
+                    {
+                        int countW = 0;
+                        for (int i = 0; i < h; i++)
+                        {
+                            if (house[i].GetType() == new Walls().GetType())
+                                countW++;
+                        }
+                        if (countW < 4)
+                        {
+                            new Worker().Work();
+                            house[h] = new Walls();
+                            new Walls().Building();
+                            h++;
+                            w++;
+                        }
+                        else
+                        {
+                            new Worker().Work();
+                            house[h] = new Door();
+                            new Door().Building();
+                            h++;
+                            w++;
+                        }
+                    }
+                    else if (house[h - 1].GetType() == new Door().GetType())
+                    {
+                        new Worker().Work();
+                        house[h] = new Window();
+                        new Window().Building();
+                        h++;
+                        w++;
+                    }
+                    else if (house[h - 1].GetType() == new Window().GetType())
+                    {
+                        int countW = 0;
+                        for (int i = 0; i < h; i++)
+                        {
+                            if (house[i].GetType() == new Window().GetType())
+                                countW++;
+                        }
+                        if (countW < 4)
+                        {
+                            new Worker().Work();
+                            house[h] = new Window();
+                            new Window().Building();
+                            h++;
+                            w++;
+                        }
+                        else
+                        {
+                            new Worker().Work();
+                            house[h] = new Roof();
+                            new Roof().Building();
+                            h++;
+                            w++;
+                        }
+                    }
+                }
+                else if (teams[w].GetType() == new TeamLeader().GetType())
+                {
+                    new TeamLeader().Work();
+                    Report(house, h);
+                    w++;
+                }
+                Console.WriteLine("=======================\n");
             }
-
-            int d = 2020;
-            car1.DateTime(ref d);
-            car1.Print();
-
-
-
-
+            house[10].Building();
 
 
             Console.Read();
