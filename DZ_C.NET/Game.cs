0075da6826c0bf7ff9e5ec4522c69b5b05cebbc5
Микола Матіос <mikolka09@gmail.com>
2020-10-09 02:50:16 +0300

    09.10.2020

---
+++ DZ_C.NET/Game.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DZ_C.NET
+{
+
+    public enum Suit { Booba = 1, Cross, Peak, Chirva };
+    public enum TypeCard { Six = 6, Seven, Eight, Nine, Ten, Jack, Lady, King, Ace };
+
+    class Game
+    {
+        private List<Player> players;
+        private Stack<Card> cards;
+
+        public void Reshuffling()
+        {
+            cards = new Stack<Card>();
+            while (cards.Count != 36)
+            {
+                Card card = new Card();
+                Random rand = new Random();
+                int su = rand.Next(1, 5);
+                if (su == 1)
+                {
+                    card.suit = Enum.GetName(typeof(Suit), su);
+                    int type = rand.Next(6, 15);
+                    if (TypeCard(type))
+                    {
+                        card.typeCard = Enum.GetName(typeof(TypeCard), type);
+                        if (SuitBool(card.suit, card.typeCard))
+                            cards.Push(card);
+                    }
+                }
+                else if (su == 2)
+                {
+                    card.suit = Enum.GetName(typeof(Suit), su);
+                    int type = rand.Next(6, 15);
+                    if (TypeCard(type))
+                    {
+                        card.typeCard = Enum.GetName(typeof(TypeCard), type);
+                        if (SuitBool(card.suit, card.typeCard))
+                            cards.Push(card);
+                    }
+                }
+                else if (su == 3)
+                {
+                    card.suit = Enum.GetName(typeof(Suit), su);
+                    int type = rand.Next(6, 15);
+                    if (TypeCard(type))
+                    {
+                        card.typeCard = Enum.GetName(typeof(TypeCard), type);
+                        if (SuitBool(card.suit, card.typeCard))
+                            cards.Push(card);
+                    }
+                }
+                else if (su == 4)
+                {
+                    card.suit = Enum.GetName(typeof(Suit), su);
+                    int type = rand.Next(6, 15);
+                    if (TypeCard(type))
+                    {
+                        card.typeCard = Enum.GetName(typeof(TypeCard), type);
+                        if (SuitBool(card.suit, card.typeCard))
+                            cards.Push(card);
+                    }
+                }
+            }
+        }
+
+        public void DistributionOfCards()
+        {
+
+        }
+
+        public int Count(string type)
+        {
+            int count = 0;
+            foreach (var item in cards)
+            {
+                if (item.typeCard == type)
+                    count++;
+            }
+            return count;
+        }
+
+        public bool SuitBool(string su, string type)
+        {
+            foreach (var item in cards)
+            {
+                if (item.typeCard == type)
+                    if (item.suit == su)
+                        return false;
+            }
+            return true;
+        }
+
+        public bool TypeCard(int type)
+        {
+            switch (type)
+            {
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    string typeCard = Enum.GetName(typeof(TypeCard), type);
+                    if (cards.Count == 0)
+                        return true;
+                    else if (Count(typeCard) < 5)
+                        return true;
+                    break;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            foreach (var fore in cards)
+            {
+                //if (fore.typeCard == NET.TypeCard.Six.ToString())
+                Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            }
+
+            //foreach (var fore in cards)
+            //{
+
+            //    if (fore.typeCard == NET.TypeCard.Seven.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Eight.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Nine.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Ten.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Jack.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Lady.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.King.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+
+            //foreach (var fore in cards)
+            //{
+            //    if (fore.typeCard == NET.TypeCard.Ace.ToString())
+            //        Console.WriteLine($"{fore.typeCard}-{fore.suit}");
+            //}
+        }
+
+
+    }
+
+    class Player
+    {
+        private Queue<Card> playerCards;
+
+        public void PrintCard()
+        {
+            foreach (var item in playerCards)
+            {
+                Console.WriteLine($"{item.typeCard}-{item.suit}");
+            }
+        }
+
+    }
+
+    class Card
+    {
+        public string suit;
+        public string typeCard;
+    }
+}
--- DZ_C.NET/Program.cs
+++ DZ_C.NET/Program.cs
@@ -9,165 +9,23 @@ using System.Threading;
 
 namespace DZ_C.NET
 {
-   class Program
+    class Program
     {
-        
+
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
 
-            
+            Game game = new Game();
+            game.Reshuffling();
+            game.Print();
 
 
-            #region BUILDING HOUSE
 
-            //Console.WriteLine("BUILDING HOUSE\n" +
-            //                  "--------------\n");
-            //House[] house = new House[11];
-            //Team[] teams = new Team[40];
-            //for (int i = 0; i < teams.Length; i++)
-            //{
-            //    System.Random rand = new Random();
-            //    int k = rand.Next(0, 2);
-
-            //    if (k == 0)
-            //        teams[i] = new Worker();
-            //    else
-            //        teams[i] = new TeamLeader();
-            //    Thread.Sleep(50);
-            //}
-            //new Team().Work();
-            //Console.WriteLine("=======================\n");
-            //Console.WriteLine();
-
-            //void Report(House[] dom, int hd)
-            //{
-            //    if(hd==0)
-            //        Console.WriteLine("Nothing built!");
-            //    else
-            //    {
-            //        for (int i = 0; i < hd; i++)
-            //        {
-            //            if (dom[i].GetType() == new Basement().GetType())
-            //                Console.WriteLine("Basement build!");
-            //            else if (dom[i].GetType() == new Walls().GetType())
-            //                Console.WriteLine("Walls build!");
-            //            else if (dom[i].GetType() == new Door().GetType())
-            //                Console.WriteLine("Door build!");
-            //            else if (dom[i].GetType() == new Window().GetType())
-            //                Console.WriteLine("Window build!");
-            //            else if (dom[i].GetType() == new Roof().GetType())
-            //                Console.WriteLine("Roof build!");
-            //        }
-            //    }
-
-            //}
-
-            //int w = 0;
-            //int h = 0;
-            //while (house[10] == null)
-            //{
-            //    if (house[0] == null)
-            //    {
-            //        if (teams[w].GetType() == new Worker().GetType())
-            //        {
-            //            new Worker().Work();
-            //            house[h] = new Basement();
-            //            new Basement().Building();
-            //            h++;
-            //            w++;
-            //        }
-            //        else if (teams[w].GetType() == new TeamLeader().GetType())
-            //        {
-            //            new TeamLeader().Work();
-            //            Report(house, h);
-            //            w++;
-            //        }
-
-            //    }
-            //    else if (teams[w].GetType() == new Worker().GetType())
-            //    {
-            //        if (house[h - 1].GetType() == new Basement().GetType())
-            //        {
-            //            new Worker().Work();
-            //            house[h] = new Walls();
-            //            new Walls().Building();
-            //            h++;
-            //            w++;
-            //        }
-            //        else if (house[h - 1].GetType() == new Walls().GetType())
-            //        {
-            //            int countW = 0;
-            //            for (int i = 0; i < h; i++)
-            //            {
-            //                if (house[i].GetType() == new Walls().GetType())
-            //                    countW++;
-            //            }
-            //            if (countW < 4)
-            //            {
-            //                new Worker().Work();
-            //                house[h] = new Walls();
-            //                new Walls().Building();
-            //                h++;
-            //                w++;
-            //            }
-            //            else
-            //            {
-            //                new Worker().Work();
-            //                house[h] = new Door();
-            //                new Door().Building();
-            //                h++;
-            //                w++;
-            //            }
-            //        }
-            //        else if (house[h - 1].GetType() == new Door().GetType())
-            //        {
-            //            new Worker().Work();
-            //            house[h] = new Window();
-            //            new Window().Building();
-            //            h++;
-            //            w++;
-            //        }
-            //        else if (house[h - 1].GetType() == new Window().GetType())
-            //        {
-            //            int countW = 0;
-            //            for (int i = 0; i < h; i++)
-            //            {
-            //                if (house[i].GetType() == new Window().GetType())
-            //                    countW++;
-            //            }
-            //            if (countW < 4)
-            //            {
-            //                new Worker().Work();
-            //                house[h] = new Window();
-            //                new Window().Building();
-            //                h++;
-            //                w++;
-            //            }
-            //            else
-            //            {
-            //                new Worker().Work();
-            //                house[h] = new Roof();
-            //                new Roof().Building();
-            //                h++;
-            //                w++;
-            //            }
-            //        }
-            //    }
-            //    else if (teams[w].GetType() == new TeamLeader().GetType())
-            //    {
-            //        new TeamLeader().Work();
-            //        Report(house, h);
-            //        w++;
-            //    }
-            //    Console.WriteLine("=======================\n");
-            //}
-            //house[10].Building();
-            #endregion
 
             Console.Read();
         }
-}
+    }
 }
