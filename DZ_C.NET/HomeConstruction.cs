525835dbe62f08392dc038166ddfb9c56d3bff3b
Микола Матіос <mikolka09@gmail.com>
2020-10-21 03:11:20 +0300

    21.10.2020

--- DZ_C.NET/HomeConstruction.cs
+++ DZ_C.NET/HomeConstruction.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Loger;
 
 namespace DZ_C.NET
 {
@@ -20,6 +21,7 @@ namespace DZ_C.NET
 
     class House : IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("\t\t\tHOUSE BUILD!!!\n" +
@@ -46,73 +48,90 @@ namespace DZ_C.NET
                I II__I  I     XXXXXXX     I
             ~~~~~'   '~~~~~~~~~~~~~~~~~~~~~~~~";
             Console.WriteLine(domik);
-
+            log.Writer(domik);
+           
         }
     }
 
     class Basement : House, IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("Building Basement!");
+            log.Writer("Building Basement!");
         }
     }
 
     class Walls : House, IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("Building Walls!");
+            log.Writer("Building Walls!");
         }
     }
 
     class Door : House, IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("Building Door!");
+            log.Writer("Building Door!");
         }
     }
 
     class Window : House, IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("Building Window!");
+            log.Writer("Building Window!");
         }
     }
 
     class Roof : House, IPart
     {
+        private LogerNew log = new LogerNew();
         public void Building()
         {
             Console.WriteLine("Building Roof!");
+            log.Writer("Building Roof!");
         }
     }
 
     class Team : IWorker
     {
+        private LogerNew log = new LogerNew();
         public void Work()
         {
             Console.WriteLine("Teams completed!");
+            log.Writer("Teams completed!");
         }
     }
 
     class Worker : Team, IWorker
     {
-
+        private LogerNew log = new LogerNew();
         public void Work()
         {
             Console.WriteLine("Worker Building!");
+            log.Writer("Worker Building!");
         }
     }
 
     class TeamLeader : Team, IWorker
     {
+        private LogerNew log = new LogerNew();
         public void Work()
         {
             Console.WriteLine("Form a Report:\n" +
                               "--------------");
+            log.Writer("Form a Report:\n" +
+                       "--------------");
         }
     }
 }
---
+++ DZ_C.NET/Payment.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    public class Payment : ISerializable
+    {
+        public static bool SerializationAll { get; set; } = true;
+        public int PayOnDay { get; set; }
+        public int CountDay { get; set; }
+        public int PenaltyOnDay { get; set; }
+        public int NumberDaysOverdue { get; set; }
+        public int Penalty { get; set; }
+        public int SumNoPenalty { get; }
+        public int SumTotal { get; }
+
+        public Payment()
+        {
+
+        }
+
+        public Payment(int PayOnDay, int CountDay, int NumberDaysOverdue, int PenaltyOnDay)
+        {
+            this.PayOnDay = PayOnDay;
+            this.CountDay = CountDay;
+            this.NumberDaysOverdue = NumberDaysOverdue;
+            this.PenaltyOnDay = PenaltyOnDay;
+            Penalty = PayOnDay * CountDay;
+            SumNoPenalty = PenaltyOnDay * NumberDaysOverdue;
+            SumTotal = Penalty + SumNoPenalty;
+        }
+
+        protected Payment(SerializationInfo info, StreamingContext context)
+        {
+            if (!SerializationAll)
+            {
+                PayOnDay = info.GetInt32("PayOnDay");
+                CountDay = info.GetInt32("CountDay");
+                PenaltyOnDay = info.GetInt32("PenaltyOnDay");
+                NumberDaysOverdue = info.GetInt32("NumberDaysOverdue");
+            }
+            else
+            {
+                PayOnDay = info.GetInt32("PayOnDay");
+                CountDay = info.GetInt32("CountDay");
+                PenaltyOnDay = info.GetInt32("PenaltyOnDay");
+                NumberDaysOverdue = info.GetInt32("NumberDaysOverdue");
+                Penalty = info.GetInt32("Penalty");
+                SumNoPenalty = info.GetInt32("SumNoPenalty");
+                SumTotal = info.GetInt32("SumTotal");
+            }
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (!SerializationAll)
+            {
+                info.AddValue("PayOnDay", PayOnDay);
+                info.AddValue("CountDay", CountDay);
+                info.AddValue("NumberDaysOverdue", NumberDaysOverdue);
+                info.AddValue("PenaltyOnDay", PenaltyOnDay);
+            }
+            else
+            {
+                info.AddValue("PayOnDay", PayOnDay);
+                info.AddValue("CountDay", CountDay);
+                info.AddValue("PenaltyOnDay", PenaltyOnDay);
+                info.AddValue("NumberDaysOverdue", NumberDaysOverdue);
+                info.AddValue("Penalty", Penalty);
+                info.AddValue("SumNoPenalty", SumNoPenalty);
+                info.AddValue("SumTotal", SumTotal);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Оплата за день:                   {PayOnDay }\n" +
+                   $"Количесвто дней:                  {CountDay }\n" +
+                   $"Штраф за один день:               {PenaltyOnDay }\n" +
+                   $"Количесвто дней задержки оплаты:  {NumberDaysOverdue }\n" +
+                   $"Сумма к оплате без штрафа:        {SumNoPenalty }\n" +
+                   $"Штраф:                            {Penalty}\n" +
+                   $"Обшая сумма к оплате:             {SumTotal }\n";
+        }
+
+    }
+}
